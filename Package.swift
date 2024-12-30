e5fdbf455831d4bbffd0ea924f93e2d3a6f3046e
Sven A. Schmidt <sas@finestructure.co>
2024-12-30 15:37:59 +0100

    Add swift-dependencies dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "dep-test",
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "dep-test",
             targets: ["dep-test"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.6.3"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "dep-test"),
+        .target(name: "dep-test", dependencies: [
+            .product(name: "Dependencies", package: "swift-dependencies"),
+        ]),
         .testTarget(
             name: "dep-testTests",
             dependencies: ["dep-test"]
