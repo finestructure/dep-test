4a8fcc1aafc407901d0841f6f2fb1e27da39febf
Sven A. Schmidt <sas@finestructure.co>
2025-01-01 09:18:30 +0100

    Bring back fetchLicense, trim down to minimum

--- Sources/dep-test/dep_test.swift
+++ Sources/dep-test/dep_test.swift
@@ -4,33 +4,13 @@ import DependenciesMacros
 
 @DependencyClient
 struct GithubClient2 {
+    var fetchLicense: @Sendable (_ owner: String, _ repository: String) async -> Github2.License?
     var fetchMetadata: @Sendable (_ owner: String, _ repository: String) async throws(Github2.Error) -> Github2.Metadata = { _, _ in XCTFail("fetchMetadata"); return .init() }
-}
-
-
-extension GithubClient2: DependencyKey {
-    static var liveValue: Self {
-        .init(
-            fetchMetadata: { _, _ in .init() }
-        )
-    }
 }
 
 
 enum Github2 {
     struct Metadata { }
+    struct License { }
     enum Error: Swift.Error { }
-}
-
-
-extension GithubClient2: TestDependencyKey {
-    static var testValue: Self { Self() }
-}
-
-
-extension DependencyValues {
-    var github2: GithubClient2 {
-        get { self[GithubClient2.self] }
-        set { self[GithubClient2.self] = newValue }
-    }
 }
